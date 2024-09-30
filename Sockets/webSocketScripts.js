e86968fc43581a367f1c377c36233f318fbcbcbb
SerhatCanBakir <serhat2534serhat@outlook.com>
2024-10-01 00:29:01 +0300

    sesli mesaj özelliği eklendi !!!!!!!!!!

--- Sockets/webSocketScripts.js
+++ Sockets/webSocketScripts.js
@@ -6,14 +6,18 @@ function initSocket(server) {
   io = socketIO(server);  
   io.on('connection', (socket) => {
     console.log('Yeni bir kullanıcı bağlandı:', socket.id);
-
+//mesajda oda user id bilgileri tokenden alınabilir veya çerezlerden alınıp ona göre işlem yapılabilir (tercihen çerez)
+//mesajlar odalara gönerdilicek odaların idleri de çerezler yada tokenden alınacak (çerez en mantıklısı)
    socket.on('message',(msj)=>{
     console.log(msj);
     mesaj = {userId:socket.id,msj:msj}
     socket.emit('mesaj al',mesaj);
    })
-  
-
+  //mesajlar seslide olabilir onun için ses alımı ve gönderimi eklenicek ve db ye eklenme yapılıcak
+socket.on('voiceMessage',(voiceMessage)=>{
+    console.log('ses geldi');
+ socket.emit('voiceMessage',voiceMessage);
+})
 
 
 
