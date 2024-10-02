abbb9275f5806ecba134db9056b26f5f624f3b15
SerhatCanBakir <serhat2534serhat@outlook.com>
2024-10-02 15:06:57 +0300

    mesajlaşma ve sesli mesajlaşma çalışır hale getirildi database eklendikten sonra ayarlamalara devam edilecek

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,8 +1,8 @@
 const {Router} = require('express');
-
+const path = require('path');
 const router = Router();
-router.get('/',(req,res)=>{
-    res.send('<h1> Hello world</h1>');
+router.get('/anasayfa',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../Htmls/holder.html'));
 })
 
 module.exports = {
