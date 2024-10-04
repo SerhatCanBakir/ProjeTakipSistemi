4bc24fb1be802f9dae6cd595668e21a5357b3a40
ismailyilmazz <ismailylmz283@gmail.com>
2024-10-04 19:55:45 +0300

    login and anasayfa incoming...

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -1,8 +1,11 @@
 const {Router} = require('express');
 const path = require('path');
 const router = Router();
-router.get('/anasayfa',(req,res)=>{
+router.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../Htmls/holder.html'));
+})
+router.get('/anasayfa',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../Htmls/anasayfa.html'));
 })
 //buraya giriş proje seçimi sayfası görev sayfası mesajlaşma ve video arama sayfası için kullanılıcak sadece 
 // kalan istekleri başka dosyada handle lıcaz 
